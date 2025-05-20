6ed1a2e6883ccc68b0d1f4bb127918ce5f540689
Clupai8o0 <clupaio4@gmail.com>
2025-05-20 22:37:59 +1000

    pls try this

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
 
 // load envs
 dotenv.config();
@@ -11,20 +11,24 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
-  res.json({ msg: "Hello" });
+	res.json({ msg: "Hello" });
 });
 
 //! Test ping route
 app.get("/ping", (req: Request, res: Response) => {
-  res.status(200).send("Ping!");
+	res.status(200).send("Ping!");
 });
 
 // running the application
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`The server is running port ${PORT}`);
-  });
+if (process.env.NODE_ENV !== "test") {
+	app.listen(
+		typeof PORT === "string" ? parseInt(PORT) : PORT,
+		"0.0.0.0",
+		() => {
+			console.log(`The server is running port ${PORT}`);
+		}
+	);
 }
 
-export default app; 
+export default app;
